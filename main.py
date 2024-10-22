75bd1814d300fcd76edb0da7babb16b2e429045f
Everson Esteves <esteveseverson@gmail.com>
2024-10-22 14:14:45 -0300

    astar implement

--- main.py
+++ main.py
@@ -1,8 +1,68 @@
-import matplotlib.pyplot as plt
-import numpy as np
+# Definindo o ponto inicial e pontos de busca
+INICIO = (18, 22)
+PERSONAGENS = [(12, 4), (34, 5), (8, 9), (37, 23), (14, 35), (36, 36)]
 
-# Define a matriz
-matriz = [
+class Node:
+    def __init__(self, x: int, y: int, custo_acumulado=0, custo_heuristico=0):
+        #localizações
+        self.x = x
+        self.y = y
+        #custos
+        self.custo_acumulado = custo_acumulado
+        self.custo_heuristico = custo_heuristico
+        self.f = custo_acumulado + custo_heuristico # f = g + h
+        
+    def __eq__(self, other) -> bool:
+        return self.x == other.x and self.y == other.y
+    
+    def __lt__(self, other) -> bool:
+        return self.f < other.f
+    
+def distancia_manhattan(ponto1: tuple, ponto2: tuple) -> int:
+    return abs(ponto1[0] - ponto2[0]) + abs(ponto1[1] - ponto2[1])
+
+def obter_vizinhos(no: tuple, matriz_terreno: list) -> list:
+    vizinhos = []
+    movimentos = [(0, 1), (1, 0), (0, -1), (-1, 0)]
+    
+    for movimento in movimentos:
+        novo_x = no.x + movimento[0]
+        novo_y = no.y + movimento[1]
+        
+        if 0 <= novo_x < len(matriz_terreno) and 0 <= novo_y < len(matriz_terreno[0]) and matriz_terreno[novo_x][novo_y] < float('inf'):
+            vizinhos.append(Node(novo_x, novo_y, 0, 0))
+            
+    return vizinhos
+
+def AStar(origem: tuple, destino: tuple, matriz_terreno: list) -> int | str:
+    lista_aberta = []
+    lista_fechada = []
+    
+    lista_aberta.append(origem)
+    
+    while lista_aberta:
+        atual = min(lista_aberta, key=lambda n: n.f)
+        
+        if atual == destino:
+            return atual.custo_acumulado
+        
+        lista_aberta.remove(atual)
+        lista_fechada.append(atual)
+        
+        for vizinho in obter_vizinhos(atual, matriz_terreno):
+            g = atual.custo_acumulado + matriz_terreno[vizinho.x][vizinho.y]
+            h = distancia_manhattan(vizinho, destino)
+            
+            if vizinho not in lista_fechada or g < vizinho.custo_acumulado:
+                if vizinho not in lista_aberta:
+                    lista_aberta.append(vizinho)
+                    
+                vizinho.custo_acumulado = g
+                vizinho.custo_heuristico = h
+                
+    return 'sem solução'
+
+matriz_terreno = [
     [5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5],
     [5, 5, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 5, 5, 5, 5, 10, 10, 10, 10, 10, 1, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5],
     [5, 5, 10, float('inf'), float('inf'), float('inf'), float('inf'), float('inf'), 10, float('inf'), float('inf'), float('inf'), float('inf'), float('inf'), float('inf'), 10, 5, 5, 5, 5, 10, float('inf'), float('inf'), float('inf'), 5, 1, 5, 5, 5, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 5, 5],
@@ -44,31 +104,9 @@ matriz = [
     [5, 5, 1, 5, float('inf'), float('inf'), float('inf'), float('inf'), 1, float('inf'), float('inf'), float('inf'), float('inf'), float('inf'), float('inf'), float('inf'), float('inf'), float('inf'), float('inf'), 5, 1, 5, 5, 5, 5, 5, 5, 10, 5, 5, 5, 5, 5, 5, 1, 10, float('inf'), float('inf'), 10, 1, 5, 5],
     [5, 5, 1, 5, 5, 5, 5, 5, 1, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 1, 5, 5, 5, 5, 5, 5, 10, 5, 5, 5, 5, 5, 5, 1, 10, 10, 10, 10, 1, 5, 5],
     [5, 5, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 5, 5],
-    [5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5]
+    [5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5]  
 ]
 
-# Cores correspondentes
-cores = {
-    1: (0.5, 0.5, 0.5),  # Cinza
-    3: (0.65, 0.33, 0.24),  # Marrom
-    5: (0.0, 0.5, 0.0),  # Verde
-    10: (1.0, 1.0, 1.0),  # Branco
-    float('inf'): (1.0, 0.65, 0.0)  # Laranja
-}
+#for i in range (len(PERSONAGENS)):
+#    print(f'Distancia entre a origem e o ponto {i}: {distancia_manhattan(INICIO, PERSONAGENS[i])}')
 
-# Definindo o ponto inicial e final (os personagens)
-inicio = (18, 22)
-personagens = [(4, 12), (5, 34), (9, 8), (23, 37), (35, 14), (36, 36)]
-#personagens = [(12, 4), (34, 5), (8, 9), (37, 23), (14, 35), (36, 36)]
-
-# Converte a matriz para um array NumPy de cores
-board_colors = np.zeros((42, 42, 3))  # Criando um array de cores
-
-for i in range(42):
-    for j in range(42):
-        board_colors[i, j] = cores.get(matriz[i][j], (1, 1, 1))
-
-# Plota o tabuleiro
-plt.imshow(board_colors, interpolation='nearest')
-plt.axis('off')  # Remove os eixos
-plt.show()
